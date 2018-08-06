71563ca2204816a00f51dd2019abc1fecbbc3cdf
adibkhan <adibmustakimkhan@hotmail.com>
2018-08-05 21:19:41 -0700

    Added console app for self hosting

---
+++ Brain.API.Console/ContainerManager.cs
@@ -0,0 +1,19 @@
+﻿using Brain.API.Managers;
+using Brain.API.Managers.Interfaces;
+using Funq;
+
+namespace Brain.API.Console
+{
+    public class ContainerManager
+    {
+        public static void Register(Container container)
+        {
+            // Autowiring dependencies
+            container.Register<IConfigManager>(x => new ConfigManager());
+            container.Register<IGroupManager>(x => new GroupManager(x.Resolve<IConfigManager>()));
+            container.Register<IUserManager>(x => new UserManager(x.Resolve<IConfigManager>()));
+            container.Register<IBrainManager>(x => new BrainManager(x.Resolve<IGroupManager>(), x.Resolve<IUserManager>()));
+
+        }
+    }
+}
---
+++ Brain.API.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using ServiceStack;
+
+namespace Brain.API.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string localHost = ConfigurationManager.AppSettings["LocalHost"];
+
+            string listeningOnPort = args.Length == 0 ? localHost : args[0];
+
+            var appHost = new AppHost()
+                .Init()
+                .Start(listeningOnPort);
+
+            System.Console.WriteLine("AppHost created at {0}, listening on {1}", DateTime.Now, listeningOnPort);
+            System.Console.ReadKey();
+        }
+    }
+}
